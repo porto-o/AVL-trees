549b0fe4312ef9f237c19d5dfb6b4ba4d5780ef3
Porto <ismael.porto2003@gmail.com>
2022-12-04 13:06:56 -0600

    Auto balanceo funcionando

--- AVL.h
+++ AVL.h
@@ -2,6 +2,16 @@
 
 typedef ArBB AVL;
 
+int absoluto(int);
+int EsAVL(AVL);
+int FactBal(AVL);
+AVL HazAVL(ArBB);
+AVL InsAVL(Elem,AVL);
+AVL rotaDer(ArBB);
+AVL rotaIzq(ArBB);
+AVL rotaDerIzq(ArBB);
+AVL rotaIzqDer(ArBB);
+
 int absoluto(int a)
 {
     if (a < 0)
@@ -9,6 +19,7 @@ int absoluto(int a)
     else
         return a;
 }
+
 int EsAVL(AVL a)
 {
     return esVacioAB(a) ||
@@ -17,7 +28,47 @@ int EsAVL(AVL a)
 }
 
 int FactBal(AVL a) { return Altura(izqAB(a)) - Altura(derAB(a)); }
-AVL HazAVL(ArBB a) { return a; }
+
+
+AVL HazAVL(ArBB a) 
+{ 
+    if(absoluto(FactBal(a)) > 1)
+    {
+        if(EsAVL(izqAB(a)) && EsAVL(derAB(a)))
+        {
+            if(FactBal(a) > 1)
+            {
+                if(FactBal(izqAB(a)) > 0)
+                    return rotaDer(a);
+                else if(FactBal(izqAB(a)) < 0)
+                    return rotaDerIzq(a);
+            }
+            else if(FactBal(a) < -1)
+            {
+                if(FactBal(derAB(a)) < 0)
+                    return rotaIzq(a);
+                else if(FactBal(derAB(a)) > 0)
+                    return rotaIzqDer(a);
+            }
+        }
+        else
+        {
+            if(EsAVL(izqAB(a)))
+                return consAB(raiz(a),izqAB(a),HazAVL(derAB(a)));
+            else if(EsAVL(derAB(a)))
+                return consAB(raiz(a),HazAVL(izqAB(a)),derAB(a));
+        }
+    }
+    else if(FactBal(a) <= 1)
+    {
+        if(EsAVL(izqAB(a)))
+            return consAB(raiz(a),izqAB(a),HazAVL(a));
+        else if(EsAVL(derAB(a)))
+            return consAB(raiz(a),HazAVL(izqAB(a)),derAB(a));
+    }    
+    
+    return a; 
+}
 
 AVL InsAVL(Elem e, AVL a)
 {
@@ -33,6 +84,7 @@ AVL rotaDer(ArBB a)
     return consAB(raiz(izqAB(a)), izqAB(izqAB(a)),
                   consAB(raiz(a), derAB(izqAB(a)), derAB(a)));
 }
+
 AVL rotaIzq(ArBB a)
 {
     return consAB(raiz(derAB(a)), consAB(raiz(a), izqAB(derAB(a)), izqAB(a)),
